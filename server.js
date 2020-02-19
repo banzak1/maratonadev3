7cac8e2a7d2a6eb59dbeb3d49aa79bf79598b7ed
Banzak <l.santana123@hotmail.com>
2020-02-19 10:16:45 -0300

    Implementação do banco de dados

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const express = require("express");
 const server = express();
 
 
+
 //configurando servidor para apresentar arquivos estaticos
 server.use(express.static('public'))
 
 //habilitar body do formulario
 server.use(express.urlencoded({extended: true}))
+
+//configurar a conexão com banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: 'leosl1230',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
 
 
 //configurando template engine
@@ -18,15 +29,6 @@ nunjucks.configure("./", {
 })
 
 //lista de doadores: array
-const donors = [
-    {
-        name: "Leonardo Santana",
-        blood: "A-"
-    },
-
-]
-
-
 
 
 
@@ -43,11 +45,21 @@ server.post("/", function(req, res){
     const email = req.body.email
     const blood = req.body.blood
 
-    //coloco valores dentro do array
-    donors.push({
-        name: name,
-        blood: blood,
-    })
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obrigatórios")
+
+    } else {
+
+    }
+
+    //coloco valores dentro do banco de dados
+    const query = `
+    INSERT INTO donors ("name", "email", "blood") 
+    VALUES ($1, $2, $3)`
+    
+    
+    
+    db.query(query, [naem, email, blood])
 
     return res.redirect("/")
 })
